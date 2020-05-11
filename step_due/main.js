1d4b97f83a81753d2be5ab15baad032c6094f4dc
Gianmarco Montanari <giammont@live.it>
2020-05-11 16:10:15 +0200

    MILESTONE-DUE(grafico agenti)

--- step_due/main.js
+++ step_due/main.js
@@ -20,64 +20,16 @@ $(document).ready(function(){
       var nomiAgenti = []; // NOMI AGENTI
       var fatturatoAgent = []; // VALORI PER GRAFICO
 
-      for (nomi in arrayDataAgent ){
+      for (var nomi in arrayDataAgent ){
         nomiAgenti.push(nomi);                         // ciclo l'array per estrapolare i dati che mi servono
         fatturatoAgent.push(arrayDataAgent[nomi]);
       };
 
 
-      var ctx = $("#chartFatturato");
-      var chartFatturato = new Chart(ctx, {
-      type: fatturatoType,
-      data: {
-          labels: mesi,
-          datasets: [{
-              label: 'VENDITE',
-              data: fatturato,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.7)',
-                  'rgba(54, 162, 235, 0.7)',
-                  'rgba(255, 206, 86, 0.7)',
-                  'rgba(75, 192, 192, 0.7)',
-                  'rgba(153, 102, 255, 0.7)',
-                  'rgba(255, 159, 64, 0.7)',
-                  'rgba(213, 42, 24, 0.7)',
-                  'rgba(164, 74, 28, 0.7)',
-                  'rgba(231, 28, 75, 0.7)',
-                  'rgba(185, 85, 36, 0.7)',
-                  'rgba(153, 217, 225, 0.7)',
-                  'rgba(217, 185, 185, 0.7)',
-                  'rgba(53, 194, 153, 0.7)',
+      stampaChart("chartFatturato", fatturatoType, mesi, fatturato);
 
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)',
-                  'rgba(213, 42, 24, 1)',
-                  'rgba(164, 74, 28, 1)',
-                  'rgba(231, 28, 75, 1)',
-                  'rgba(185, 85, 36, 1)',
-                  'rgba(153, 217, 225, 1)',
-                  'rgba(217, 185, 185, 1)',
-                  'rgba(53, 194, 153, 1)',
-              ],
-              borderWidth: 2
-                  }]
-              },
-              options: {
-                  scales: {
-                      yAxes: [{
-                          ticks: {
-                              beginAtZero: true
-                          }
-                      }]
-                  }
-              }
-        }); //fine mychart
+      stampaChart("chartAgenti", fatturatoAgentType, nomiAgenti, fatturatoAgent);
+
 
 
 
@@ -90,3 +42,63 @@ $(document).ready(function(){
 
 
 }); //fine document ready
+
+
+// FUNZIONI GENERICHE
+
+function stampaChart(id, tipoGrafico, labels, data){
+  var ctx = $("#" + id );
+  var id = new Chart(ctx, {
+    type: tipoGrafico,
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'VENDITE',
+            data: data,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.7)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(255, 206, 86, 0.7)',
+                'rgba(75, 192, 192, 0.7)',
+                'rgba(153, 102, 255, 0.7)',
+                'rgba(255, 159, 64, 0.7)',
+                'rgba(213, 42, 24, 0.7)',
+                'rgba(164, 74, 28, 0.7)',
+                'rgba(231, 28, 75, 0.7)',
+                'rgba(185, 85, 36, 0.7)',
+                'rgba(153, 217, 225, 0.7)',
+                'rgba(217, 185, 185, 0.7)',
+                'rgba(53, 194, 153, 0.7)',
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(213, 42, 24, 1)',
+                'rgba(164, 74, 28, 1)',
+                'rgba(231, 28, 75, 1)',
+                'rgba(185, 85, 36, 1)',
+                'rgba(153, 217, 225, 1)',
+                'rgba(217, 185, 185, 1)',
+                'rgba(53, 194, 153, 1)',
+            ],
+            borderWidth: 2
+                }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+
+    }); //fine mychart
+
+} //FINE FUNZIONE STAMPACHART
