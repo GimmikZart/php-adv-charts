939fe1ff38d7c48a3ee26f17f1108348eea28921
Gianmarco Montanari <giammont@live.it>
2020-05-11 15:30:16 +0200

    MILESTONE-DUE (grafico fatturato)

---
+++ step_due/main.js
@@ -0,0 +1,92 @@
+
+$(document).ready(function(){
+
+  $.ajax({
+    url: "server.php",
+    method: "GET",
+    success: function(data){
+
+      var mesi = ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'];
+
+      // GRAFICO UNO
+      var fatturatoType = data.fatturato["type"]; // TIPO DI GRAFICO
+      var fatturato = data.fatturato["data"]; // VALORI PER GRAFICO
+
+      // GRAFICO DUE
+      var fatturatoAgentType = data.fatturato_by_agent["type"]; // TIPO DI GRAFICO
+
+      var arrayDataAgent = data.fatturato_by_agent["data"]; //grabbo il data array
+
+      var nomiAgenti = []; // NOMI AGENTI
+      var fatturatoAgent = []; // VALORI PER GRAFICO
+
+      for (nomi in arrayDataAgent ){
+        nomiAgenti.push(nomi);                         // ciclo l'array per estrapolare i dati che mi servono
+        fatturatoAgent.push(arrayDataAgent[nomi]);
+      };
+
+
+      var ctx = $("#chartFatturato");
+      var chartFatturato = new Chart(ctx, {
+      type: fatturatoType,
+      data: {
+          labels: mesi,
+          datasets: [{
+              label: 'VENDITE',
+              data: fatturato,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.7)',
+                  'rgba(54, 162, 235, 0.7)',
+                  'rgba(255, 206, 86, 0.7)',
+                  'rgba(75, 192, 192, 0.7)',
+                  'rgba(153, 102, 255, 0.7)',
+                  'rgba(255, 159, 64, 0.7)',
+                  'rgba(213, 42, 24, 0.7)',
+                  'rgba(164, 74, 28, 0.7)',
+                  'rgba(231, 28, 75, 0.7)',
+                  'rgba(185, 85, 36, 0.7)',
+                  'rgba(153, 217, 225, 0.7)',
+                  'rgba(217, 185, 185, 0.7)',
+                  'rgba(53, 194, 153, 0.7)',
+
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(213, 42, 24, 1)',
+                  'rgba(164, 74, 28, 1)',
+                  'rgba(231, 28, 75, 1)',
+                  'rgba(185, 85, 36, 1)',
+                  'rgba(153, 217, 225, 1)',
+                  'rgba(217, 185, 185, 1)',
+                  'rgba(53, 194, 153, 1)',
+              ],
+              borderWidth: 2
+                  }]
+              },
+              options: {
+                  scales: {
+                      yAxes: [{
+                          ticks: {
+                              beginAtZero: true
+                          }
+                      }]
+                  }
+              }
+        }); //fine mychart
+
+
+
+    }, // fine success
+    error: function(error){
+      alert("ERRORE" + error);
+    },
+
+  }); // fine chiamata ajax
+
+
+}); //fine document ready
---
+++ step_due/server.php
@@ -0,0 +1,10 @@
+
+<?php
+  // ZONA DI PRESA DEI DATI
+    header('Content-Type: application/json');
+
+      include "database.php";
+
+    echo json_encode($graphs);
+
+ ?>
